29f42b701bd59b913689f828477836f1b8e7d7a0
alperbok <alper78300@gmail.com>
2026-09-23 15:11:25 +0200

    Add libcomlair-category-engine-v173.js

---
+++ libcomlair-category-engine-v173.js
@@ -0,0 +1,14 @@
+(function(){
+  "use strict";
+  window.LibcomlairCategories=Object.freeze({
+    categories:["Tous","Restaurants","Hôtels","Bars","Magasins","Loisirs","Services","Transports"],
+    shopTypes:["Tous les magasins","Alimentation","Vêtements","Pharmacie / parapharmacie","Centre commercial","Maison / bricolage","Électronique","Beauté","Culture / loisirs"],
+    transportTypes:["Tous les transports","🚆 Train / Gare","🚌 Bus / Arrêt","🚇 Métro","🚊 Tramway","🚕 Taxi / transport adapté","⛴️ Bateau / ferry"],
+    serviceTypes:["Tous les services","Santé / soins","Administrations / services publics","Banques","Poste","Services sociaux","Aide à domicile","Toilettes publiques accessibles","Accueil / information","Services spécialisés handicap"],
+    leisureTypes:["Tous les loisirs","Cinéma","Musée","Théâtre / spectacle","Bibliothèque / médiathèque","Sport","Piscine","Parc / jardin","Parc d’attractions","Activités culturelles"],
+    restaurantTypes:["Tous les restaurants","Restaurant traditionnel","Fast-food","Café / salon de thé","Pizzeria","Cuisine du monde","Gastronomique"],
+    hotelTypes:["Tous les hébergements","Hôtel","Appart’hôtel","Auberge / hostel","Chambre d’hôtes","Résidence de tourisme","Camping / hébergement de plein air"],
+    barTypes:["Tous les bars","Bar / café","Pub","Bar à cocktails","Bar musical","Bar d’hôtel","Bar-restaurant"],
+    accessibilityFilters:["Entrée sans marche","Toilettes accessibles","Ascenseur","Stationnement adapté","Chambre accessible","Guidage tactile","Bandes d’éveil à la vigilance","Balises sonores","Braille ou relief","Information visuelle","Boucle magnétique","Signalétique simplifiée","Orientation facilitée","Personnel disponible","Assistance sur demande","Chien guide / d’assistance accepté","Espace calme disponible"]
+  });
+})();
