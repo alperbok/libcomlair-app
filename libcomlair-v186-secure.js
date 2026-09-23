de2983413c782fc3dff5df362efe9026d522c128
alperbok <alper78300@gmail.com>
2026-09-24 00:50:15 +0200

    Fix copied line breaks in V186 application script

--- libcomlair-v186-secure.js
+++ libcomlair-v186-secure.js
@@ -339,7 +339,10 @@ function startVisionIntroSpeech(fromGesture=false){
   if(window.__libcomlairVisionIntroPlayed)return true;
   const engine=window.LibcomlairVoice;
   if(!engine||!engine.available)return false;
-  const ok=engine.speak(visionIntroText(),{\n    rate:0.9,\n    naturalOnlyUntilGesture:!fromGesture,\n    onstart:()=>{
+  const ok=engine.speak(visionIntroText(),{
+    rate:0.9,
+    naturalOnlyUntilGesture:!fromGesture,
+    onstart:()=>{
       window.__libcomlairVisionIntroPlayed=true;
       removeVisionIntroFallback();
     },
