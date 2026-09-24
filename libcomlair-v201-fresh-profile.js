b7d05e4d69d623026d1599db74eef5b69e5219f8
alperbok <alper78300@gmail.com>
2026-09-24 14:50:26 +0200

    Force V201 to start on fresh accessibility profile chooser

---
+++ libcomlair-v201-fresh-profile.js
@@ -0,0 +1,8 @@
+(function(){
+  "use strict";
+  try{localStorage.removeItem("libcomlair-access-profile-v1")}catch(_){}
+  try{
+    window.__libcomlairVisionIntroPlayed=false;
+    window.__libcomlairVisionIntroFallback=null;
+  }catch(_){}
+})();
