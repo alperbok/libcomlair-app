9b2f2e34629395db882be2cb7fdae745f1bff051
alperbok <alper78300@gmail.com>
2026-09-24 13:56:17 +0200

    Add V195 service worker

---
+++ libcomlair-sw-v195.js
@@ -0,0 +1,49 @@
+const CACHE="libcomlair-v195-shell";
+const START="./test-v195-vision-app.html";
+const ASSETS=[
+  START,
+  "./style.css",
+  "./libcomlair-icon-v195.svg",
+  "./libcomlair-render-voice-v195.js",
+  "./libcomlair-voice-engine-v189.js",
+  "./libcomlair-ui-v188.js",
+  "./libcomlair-category-engine-v173.js",
+  "./libcomlair-accessibility-engine-v173.js",
+  "./libcomlair-data-engine-v173.js",
+  "./libcomlair-detail-engine-v173.js",
+  "./libcomlair-transport-engine-v173.js",
+  "./libcomlair-repair-engine-v175.js",
+  "./libcomlair-selftest-v189.js",
+  "./libcomlair-v195-secure.js",
+  "./libcomlair-v157-directions.js"
+];
+self.addEventListener("install",event=>{
+  self.skipWaiting();
+  event.waitUntil((async()=>{
+    const cache=await caches.open(CACHE);
+    await cache.add(START);
+    await Promise.allSettled(ASSETS.filter(x=>x!==START).map(x=>cache.add(x)));
+  })());
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil((async()=>{
+    const keys=await caches.keys();
+    await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin)return;
+  event.respondWith((async()=>{
+    try{
+      const fresh=await fetch(event.request);
+      const cache=await caches.open(CACHE);
+      cache.put(event.request,fresh.clone()).catch(()=>{});
+      return fresh;
+    }catch(_){
+      return (await caches.match(event.request)) || (await caches.match(START));
+    }
+  })());
+});
